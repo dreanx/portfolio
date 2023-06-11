72b0faf42728ce244046350541e00c2466d5ac51
dreanx <pierre.basck@gmail.com>
2023-06-11 12:35:25 +0200

    before Project Component

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       <h1 className="font-bangers text-4xl bg-red-500 text-center py-10">
         Welcome to my page!
       </h1>
-      <p className="font-lato text-lg p-5">
+      <p className="font-lato text-lg px-5 py-10 bg-blue-300">
         This is a mobile-first approach for the home page content. Adjustments
         for larger screens can be made using Tailwind CSS responsive classes.
       </p>
