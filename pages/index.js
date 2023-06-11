7af8fe1a5491edaac70ae20bf221530e1691d11d
dreanx <pierre.basck@gmail.com>
2023-06-11 10:17:30 +0200

    import fonts

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,9 @@ import Nav from '../components/nav';
 
 export default function Home() {
   return (
-    <div className="bg-purple-900">
+    <div className="font-bangersbg-purple-900">
       <Nav />
-      <h1 className="bg-red-500 text-white text-center py-8 text-4xl font-bold">
+      <h1 className="font-bangers text-6xl bg-red-500 text-white text-center py-8 text-4xl">
         Welcome to my page!
       </h1>
     </div>
